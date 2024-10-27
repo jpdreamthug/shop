85885942a2a78cfd1608bc9b5d4f72a026e6cb60
jpdreamthug <lentorilp@gmail.com>
2024-10-27 14:00:02 +0200

    implement custom middleware for using JWT in WS connection

--- backend/shop_service/asgi.py
+++ backend/shop_service/asgi.py
@@ -1,16 +1,20 @@
-"""
-ASGI config for shop_service project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
-"""
-
 import os
 
+import django
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shop_service.settings')
+from notifications import routing
+from shop_service.middleware import TokenAuthMiddleware
 
-application = get_asgi_application()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shop_service.settings')
+django.setup()
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    'websocket': TokenAuthMiddleware(
+        URLRouter(
+            routing.websocket_urlpatterns
+        )
+    ),
+})
---
+++ backend/shop_service/middleware.py
@@ -0,0 +1,56 @@
+import os
+from datetime import datetime
+
+import django
+from django.contrib.auth import get_user_model
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shop_service.settings')
+django.setup()
+
+import jwt
+from django.conf import settings
+from django.contrib.auth.models import AnonymousUser
+from channels.db import database_sync_to_async
+from channels.middleware import BaseMiddleware
+
+from django.db import close_old_connections
+
+ALGORITHM = "HS256"
+
+User = get_user_model()
+
+@database_sync_to_async
+def get_user(token):
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=ALGORITHM)
+        print('payload', payload)
+    except:
+        print('no payload')
+        return AnonymousUser()
+
+    token_exp = datetime.fromtimestamp(payload['exp'])
+    if token_exp < datetime.utcnow():
+        print("no date-time")
+        return AnonymousUser()
+
+    try:
+        user = User.objects.get(id=payload['user_id'])
+        print('user', user)
+    except User.DoesNotExist:
+        print('no user')
+        return AnonymousUser()
+
+    return user
+
+
+class TokenAuthMiddleware(BaseMiddleware):
+
+    async def __call__(self, scope, receive, send):
+        close_old_connections()
+        try:
+            token_key = (dict((x.split('=') for x in scope['query_string'].decode().split("&")))).get('token', None)
+        except ValueError:
+            token_key = None
+        scope['user'] = await get_user(token_key)
+        print('d2', scope['user'])
+        return await super().__call__(scope, receive, send)
