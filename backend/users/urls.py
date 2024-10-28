cf1d124b76056c46be980338f928ada508f71616
jpdreamthug <lentorilp@gmail.com>
2024-10-28 23:26:15 +0200

    rename path

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -9,6 +9,6 @@ app_name = "users"
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
     path("me/", ManagerUserView.as_view(), name="manage_user"),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
